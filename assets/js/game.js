3342cd8ea28d8ba8e27785c1dc0a46147f89bd82
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-08-16 23:30:12 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -59,12 +59,57 @@ var fight = function (enemyName) {
   }
 };
 
+var startGame = function () {
+
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
   for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+      // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      // pick new enemy to fight based on the index of the enemyNames array
+      var pickedEnemyName = enemyNames[i];
+
+      // reset enemyHealth before starting new fight
+      enemyHealth = 50;
+
+      // use debugger to pause script from running and check what's going on at that moment in the code
+      // debugger;
+
+      // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+      fight(pickedEnemyName);
+    } else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+  }
+  endGame();
 }
 
+var endGame = function () {
+  window.alert("The game has now ended. Let's see how you did!");
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    // restart the game
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+}
+
+var shop = function () { }
 
 
-
+startGame();
